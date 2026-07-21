4627d3bf29cfd8ffd981b65bf593fc304fe502ac
Hoang Nguyen <hoangbkit@gmail.com>
2026-07-21 09:04:55 +0700

    Apply purchase mode only at app launch

--- StarterApp/App/StarterAppApp.swift
+++ StarterApp/App/StarterAppApp.swift
@@ -7,11 +7,6 @@ struct StarterAppApp: App {
     @State private var purchases = AppConfiguration.makePurchaseController()
     @State private var themes = ThemeManager(catalog: .foundationDefaults)
 
-    #if DEBUG
-    @AppStorage(AppConfiguration.simulatedPurchaseModeDefaultsKey)
-    private var simulatedPurchasesEnabled = AppConfiguration.isSimulatedPurchaseModeEnabled
-    #endif
-
     var body: some Scene {
         WindowGroup {
             AppRootView()
@@ -20,19 +15,6 @@ struct StarterAppApp: App {
                 .managesPurchases(purchases)
                 .appFoundationTheme(themes)
                 .synchronizesThemeAccess(themes, hasPro: purchases.isEntitled)
-                #if DEBUG
-                .task(id: simulatedPurchasesEnabled) {
-                    guard purchases.isUsingSimulatedPurchases != simulatedPurchasesEnabled else {
-                        return
-                    }
-
-                    let replacement = AppConfiguration.makePurchaseController(
-                        simulated: simulatedPurchasesEnabled
-                    )
-                    purchases = replacement
-                    await replacement.prepare()
-                }
-                #endif
         }
     }
 }
