a78a5108b6a2f1af1fb9db49fc3fa8bb2f71ac36
Hoang Nguyen <hoangbkit@gmail.com>
2026-07-21 08:47:12 +0700

    Reload purchases when debug mode changes

--- StarterApp/App/StarterAppApp.swift
+++ StarterApp/App/StarterAppApp.swift
@@ -7,6 +7,11 @@ struct StarterAppApp: App {
     @State private var purchases = AppConfiguration.makePurchaseController()
     @State private var themes = ThemeManager(catalog: .foundationDefaults)
 
+    #if DEBUG
+    @AppStorage(AppConfiguration.simulatedPurchaseModeDefaultsKey)
+    private var simulatedPurchasesEnabled = AppConfiguration.isSimulatedPurchaseModeEnabled
+    #endif
+
     var body: some Scene {
         WindowGroup {
             AppRootView()
@@ -15,6 +20,19 @@ struct StarterAppApp: App {
                 .managesPurchases(purchases)
                 .appFoundationTheme(themes)
                 .synchronizesThemeAccess(themes, hasPro: purchases.isEntitled)
+                #if DEBUG
+                .task(id: simulatedPurchasesEnabled) {
+                    guard purchases.isUsingSimulatedPurchases != simulatedPurchasesEnabled else {
+                        return
+                    }
+
+                    let replacement = AppConfiguration.makePurchaseController(
+                        simulated: simulatedPurchasesEnabled
+                    )
+                    purchases = replacement
+                    await replacement.prepare()
+                }
+                #endif
         }
     }
 }
