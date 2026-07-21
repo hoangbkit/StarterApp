a8e8d7cf85f330cf5e9a8d2417e09805102a76fa
Hoang Nguyen <hoangbkit@gmail.com>
2026-07-21 17:01:53 +0700

    Install themed StarterApp shell

--- StarterApp/App/StarterAppApp.swift
+++ StarterApp/App/StarterAppApp.swift
@@ -1,11 +1,22 @@
 import AppFoundation
 import SwiftUI
+import UIKit
 
 @main
 @MainActor
 struct StarterAppApp: App {
-    @State private var purchases = AppConfiguration.makePurchaseController()
+    @State private var purchases = AppConfiguration.makePurchaseManager()
     @State private var themes = ThemeManager(catalog: .foundationDefaults)
+
+    init() {
+        let navigationBar = UINavigationBar.appearance()
+        navigationBar.largeTitleTextAttributes = [
+            .font: Self.roundedSystemFont(size: 34, weight: .bold),
+        ]
+        navigationBar.titleTextAttributes = [
+            .font: Self.roundedSystemFont(size: 17, weight: .semibold),
+        ]
+    }
 
     var body: some Scene {
         WindowGroup {
@@ -14,7 +25,16 @@ struct StarterAppApp: App {
                 .environment(themes)
                 .managesPurchases(purchases)
                 .appFoundationTheme(themes)
-                .synchronizesThemeAccess(themes, hasPro: purchases.isEntitled)
+                .synchronizesThemeAccess(themes, hasPro: purchases.hasPro)
+                .environment(\.font, .system(.body, design: .rounded))
         }
+    }
+
+    private static func roundedSystemFont(size: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let systemFont = UIFont.systemFont(ofSize: size, weight: weight)
+        guard let roundedDescriptor = systemFont.fontDescriptor.withDesign(.rounded) else {
+            return systemFont
+        }
+        return UIFont(descriptor: roundedDescriptor, size: size)
     }
 }
